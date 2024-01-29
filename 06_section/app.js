a13c6a8c7551b26363a7208fa2b92c8f8804fb6c
unknown <imamovic.kerim@gmail.com>
2024-01-29 15:23:14 +0100

    izmjene 3 - section 5 i section 6
    -kreirani folderi 05_section i 06_section
    -instalirani paketu u terminalu: dotenv, express, mongoose, nodemon
    -kreiran .gitignore fajl
    -kreiran app.js fajl
    -kreirani folderi: controllers, db, errors, middleware, models, routes i js fajlovi unutar njih
    -kreiran public folder za frontend
    -kreiran section.txt fajl
    -potrebno kreirati .env fajl

---
+++ 06_section/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
+
+// routes
+app.get('/hello', (req, res) => { res.send('Task Manager App') });
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    }
+    catch (error) {
+        console.error(error);
+    };
+};
+
+start();
