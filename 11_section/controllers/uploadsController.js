81f085a3fcaad0e14fe22d930888cadb7865919c
unknown <imamovic.kerim@gmail.com>
2024-02-17 23:14:02 +0100

    izmjene 8 - section 11
    -kreiran folder 11_section
    -instaliramo pakete u terminalu
    -kreiran .gitignore fajl
    -kreiran app.js fajl
    -kreirani folderi: controllers, db, errors middleware, models, routes i js fajlovi u njima
    -kreiran folder images sa slikama
    -kreiran folder public i fajlovi u njemu
    -kreiran section.txt fajl
    -potrebno kreirati .env fajl sa varijablom MONGO_URI

---
+++ 11_section/controllers/uploadsController.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
+const uploadProductImage = async (req, res) => {
+    if (!req.files) throw new CustomError.BadRequestError('No file uploaded!');
+    const productImage = req.files.image;
+    
+    if (!productImage.mimetype.startsWith('image')) throw new CustomError.BadRequestError('Please upload image!')
+
+    const maxSize = 1024 * 1024;
+    if (productImage.size > maxSize) throw new CustomError.BadRequestError('Please upload image smaller than 1KB ')
+    
+
+    const imagePath = path.join(__dirname, '../public/uploads/'+`${productImage.name}`);
+    
+    await productImage.mv(imagePath);
+    
+    return res.status(StatusCodes.OK).json({ imgage: { src: `/uploads/${productImage.name}`}});
+};
+
+module.exports = { uploadProductImage };
